89a31a462498e6782f7e83f4ee70c6c766b5d0b8
Giorgio-Margarone <49938162+giorgio-marga@users.noreply.github.com>
2021-10-28 16:35:53 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c2b36a36fe5d4849c60a413453279d99",
-"index.html": "d05b277b58cf1089368c86ec6e6c1c8f",
-"/": "d05b277b58cf1089368c86ec6e6c1c8f",
-"main.dart.js": "97786bb56efad3ed43cdbf10ab362fed",
+"index.html": "33a58b6f9585ea641968294b807df1b6",
+"/": "33a58b6f9585ea641968294b807df1b6",
+"main.dart.js": "90da51b054a0941984aec184e4357cfe",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
